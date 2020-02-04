c62d1ceb766d3ef5ce89da1e1425cac2182409f6
aleibets <amplejoe@gmail.com>
2020-02-04 19:32:13 +0100

    new functionality: object memory analysis

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@
 # @Email:  aleibets@itec.aau.at
 # @Filename: utils.py
 # @Last modified by: aleibets
-# @Last modified time: 2020-01-27T12:08:47+01:00
+# @Last modified time: 2020-02-04T12:02:53+01:00
 # @description:  Utility class for common python tasks
 # @notes:  requires ntpath, natsort, shutil
 
@@ -280,6 +280,30 @@ def copy_to(src_path, dst_path, follow_symlinks=True):
         (or overwritten) in the directory specified.
     """
     shutil.copy(src_path, dst_path, follow_symlinks=follow_symlinks)
+
+# # MEMORY
+
+
+# from: https://goshippo.com/blog/measure-real-size-any-python-object/
+def get_size(obj, seen=None):
+    """Recursively finds size of objects in bytes"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
 
 
 # # MISCELLANEOUS
