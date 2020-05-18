784ad8678c9fb545ea8519805f32eebf6f558738
unknown <amplejoe@gmail.com>
2020-05-18 20:48:40 +0200

    pixel mean/std calculation

--- cnn_utils.py
+++ cnn_utils.py
@@ -3,6 +3,8 @@ from . import utils
 import copy
 import itertools
 import functools
+import cv2
+import numpy as np
 
 # detectron2 imports
 from detectron2.data.datasets import register_coco_instances
@@ -12,6 +14,7 @@ ctypes.cdll.LoadLibrary('caffe2_nvrtc.dll')
 
 SETTINGS_FILE = "settings.json"
 IN_IMG_FOLDERS = ['img', 'imag', 'frame', 'pic', 'phot']
+IN_IMG_EXT = ['.jpeg', '.jpg', '.png', '.bmp']
 
 COCO = "coco.json"
 COCO_TRAIN = "coco_train.json"
@@ -85,10 +88,9 @@ def set_d2_cfg_attr(cfg, setting, evaluate=False):
 def register_datasets(ds_info):
     """ WARNING: don't do this multiple times
     """
-    img_folder = utils.prompt_folder_confirm(ds_info['ds_path'], IN_IMG_FOLDERS, 'images')
-    register_coco_instances(f"{ds_info['ds_name']}_train", {}, ds_info["ds_train"], img_folder)
-    register_coco_instances(f"{ds_info['ds_name']}_val", {}, ds_info["ds_val"], img_folder)
-    register_coco_instances(f"{ds_info['ds_name']}_test", {}, ds_info["ds_test"], img_folder)
+    register_coco_instances(f"{ds_info['ds_name']}_train", {}, ds_info["ds_train"], ds_info['image_path'])
+    register_coco_instances(f"{ds_info['ds_name']}_val", {}, ds_info["ds_val"], ds_info['image_path'])
+    register_coco_instances(f"{ds_info['ds_name']}_test", {}, ds_info["ds_test"], ds_info['image_path'])
 
 
 def load_d2_cfg(ds_info, config_file):
@@ -99,6 +101,80 @@ def load_d2_cfg(ds_info, config_file):
     return config
 
 
+# based on https://gist.github.com/jdhao/9a86d4b9e4f79c5330d54de991461fd6
+def calc_pixel_mean_std(ds_info, num_channels=3):
+    """ Calculates the pixel mean of the input dataset in BGR format.
+        Return : tuple(list[bgr_mean], list[bgr_std])
+    """
+    img_paths = utils.get_file_paths(ds_info['image_path'], *IN_IMG_EXT)
+    channel_sum = np.zeros(num_channels)
+    channel_sum_squared = np.zeros(num_channels)
+
+    pixel_num = 0  # store all pixel number in the dataset
+    for file in tqdm(img_paths, desc="Calculating DS pixel mean"):
+        im = cv2.imread(file)  # image in M*N*CHANNEL_NUM shape, channel in BGR order
+        im = im/255.0
+        pixel_num += (im.size/num_channels)
+        channel_sum += np.sum(im, axis=(0, 1))
+        channel_sum_squared += np.sum(np.square(im), axis=(0, 1))
+
+    bgr_mean_normalized = channel_sum / pixel_num
+    bgr_std_normalized = np.sqrt(channel_sum_squared / pixel_num - np.square(bgr_mean_normalized))
+
+    # unnormalize ()
+    bgr_mean = bgr_mean_normalized * 255.0
+    bgr_std = bgr_std_normalized * 255.0
+
+    # change the format from bgr to rgb
+    # rgb_mean = list(bgr_mean)[::-1]
+    # rgb_std = list(bgr_std)[::-1]
+
+    # limit to 3 decimals
+    return ([float(round(x, 3)) for x in list(bgr_mean)]), [float(round(x, 3)) for x in list(bgr_std)]
+
+
+def calc_min_max_pixel_vals(ds_info, num_channels=3):
+    img_paths = utils.get_file_paths(ds_info['image_path'], *IN_IMG_EXT)
+    max_total = np.zeros((num_channels, 1))
+    min_total = np.zeros((num_channels, 1))
+    for file in tqdm(img_paths, desc="Calculating min max values"):
+        image = cv2.imread(file)
+        # max, min channel values
+        # tmp = [0] * num_channels
+        bgr_tuple = cv2.split(image)  # b, g, r
+        max_tmp = list([np.amax(x) for x in bgr_tuple])
+        min_tmp = list([np.amin(x) for x in bgr_tuple])
+        for i, val in enumerate(max_tmp):
+            max_total[i] = max_tmp[i] if max_total[i] < max_tmp[i] else max_total[i]
+        for i, val in enumerate(min_tmp):
+            min_total[i] = min_tmp[i] if min_total[i] > min_tmp[i] else min_total[i]
+    return list(min_total.flatten()), list(max_total.flatten())
+
+
+# # wrongly calcs std (over all images instead of over all pixels)
+# def calc_pixel_mean_std_wrong(ds_info, num_channels=3):
+#     """ Calculates the pixel mean of the input dataset in BGR format.
+#         Return : tuple(list[bgr_mean], list[bgr_std])
+#     """
+#     img_paths = utils.get_file_paths(ds_info['image_path'], *IN_IMG_EXT)
+#     count = 0
+#     mean_total = np.zeros((num_channels, 1))
+#     std_total = np.zeros((num_channels, 1))
+#
+#     for file in tqdm(img_paths, desc="Calculating pixel mean"):
+#         image = cv2.imread(file)
+#         # BGR
+#         mean_img, std_img = cv2.meanStdDev(image)
+#         mean_total += mean_img
+#         std_total += std_img
+#         count += 1
+#
+#     mean_total /= count
+#     std_total /= count
+#
+#     return list(mean_total.flatten()), list(std_total.flatten())
+
+
 def create_d2_cfgs(ds_info, d2_configs_root, cnns, parameters):
     """ Get detectron2 configs for a specific dataset. Returns list of cfgs depending on parameters
     """
@@ -106,21 +182,17 @@ def create_d2_cfgs(ds_info, d2_configs_root, cnns, parameters):
     coco_ds = utils.read_json(ds_info["ds_train"])
     num_classes = len(coco_ds["categories"])
 
-    # script_dir = utils.get_script_dir()
-    # global_settings_path = utils.join_paths_str(script_dir, GLOBAL_SETTINGS)
-    # global_settings = utils.read_json(global_settings_path)
-
     cnn_cfgs = {}
     for cnn in cnns:
         cnn_cfgs[cnn["name"]] = []
         base_cfg = get_cfg()
 
-        # D2 settings
+        # D2 config
         if is_key_set(cnn, "cfg_url"):
             base_cfg.merge_from_file(
                 utils.join_paths_str(d2_configs_root, cnn["cfg_url"])
             )
-        # my base settings
+        # my base config (e.g. base_configs/cfg_lz.yaml)
         base_cfg.merge_from_file(
             ds_info["base_cfg"]
         )
@@ -132,9 +204,10 @@ def create_d2_cfgs(ds_info, d2_configs_root, cnns, parameters):
             base_cfg.MODEL.WEIGHTS = cnn["weight_url"]
         base_cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes
 
-        # for setting in global_settings:
-        #     set_d2_cfg_attr(base_cfg, setting, is_key_enabled(setting, "eval"))
-        # params_dict = params_list_to_dict(parameters)
+        # ds pixel mean, pixel std
+        px_mean, px_std = calc_pixel_mean_std(ds_info)
+        base_cfg.MODEL.PIXEL_MEAN = px_mean
+        base_cfg.MODEL.PIXEL_STD = px_std
 
         # get cartesian product of all parameters
         param_permuts = list(cart_product_dict(**parameters))
@@ -168,7 +241,5 @@ def get_ds_info(ds_path, base_cfg=None):
     dataset_info["ds_test"] = utils.join_paths_str(ds_path, COCO_TEST)
     if base_cfg:
         dataset_info["base_cfg"] = base_cfg
-    # settings_file = utils.join_paths_str(dataset_info["ds_path"], SETTINGS_FILE)
-    # ds_settings = utils.read_json(settings_file)
-    # ds_settings = {**dataset_info, **ds_settings}  # merge dicts
+    dataset_info["image_path"] = utils.prompt_folder_confirm(dataset_info["ds_path"], IN_IMG_FOLDERS, 'images')
     return dataset_info
