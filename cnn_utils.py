eab4b64892697f2548a99a937e54b9b05f74d59b
unknown <amplejoe@gmail.com>
2020-05-21 16:10:09 +0200

    plot improvements and revised validation strategy

--- cnn_utils.py
+++ cnn_utils.py
@@ -266,6 +266,7 @@ def create_d2_cfgs(ds_info, script_dir):
 
         # set max_iter to resemple ds_info['cfg']['training']['num_epochs']
         one_epoch = ds_info["num_train_images"] / base_cfg.SOLVER.IMS_PER_BATCH
+        base_cfg.TEST.EVAL_PERIOD = int(one_epoch)  # validate after on epoch
         base_cfg.SOLVER.MAX_ITER = int(one_epoch * ds_info['cfg']['training']['num_epochs'])
 
         for perm in param_permuts:
--- plot_utils.py
+++ plot_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from . import utils
 from . import cnn_utils
 
@@ -25,11 +26,16 @@ def test_plot():
 
 
 # orig src: https://gist.github.com/ortegatron/c0dad15e49c2b74de8bb09a5615d9f6b#file-mytrainer-py
-def save_train_val_loss(model_folder, num_train_images, batch_size, show=False):
+def save_train_val_loss(model_folder, num_train_images, batch_size, out_file="train_val_loss.jpg", show=False):
     experiment_metrics = utils.read_json_arr(model_folder + '/metrics.json')
 
     # change fig size before 'plot'
-    plt.figure(figsize=(4, 3), dpi=300)
+    plt.figure(figsize=(4, 3), dpi=300, num=out_file).gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # plt.title(out_file)
+    # plt.figure(num=out_file)
+    # fig = plt.figure()
+    # fig.canvas.set_window_title(out_file)
     # plot (epochs instead of just iterations)
     plt.plot(
         [cnn_utils.iter_to_epoch(x['iteration'], num_train_images, batch_size) for x in experiment_metrics],
@@ -38,7 +44,7 @@ def save_train_val_loss(model_folder, num_train_images, batch_size, show=False):
         [cnn_utils.iter_to_epoch(x['iteration'], num_train_images, batch_size) for x in experiment_metrics if 'validation_loss' in x],
         [x['validation_loss'] for x in experiment_metrics if 'validation_loss' in x])
     plt.legend(['training', 'validation'], loc='upper right')
-    out_path = utils.join_paths_str(model_folder, "train_val_loss.jpg")
+    out_path = utils.join_paths_str(model_folder, out_file)
     plt.xlabel("epoch")
     plt.ylabel("loss (smooth-L1)")
     plt.tight_layout()  # prevent x/y labels to be cut off
