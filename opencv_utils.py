1b7de1ce938173d6f4943bd370d22aba7be51b00
unknown <amplejoe@gmail.com>
2020-06-23 17:25:19 +0200

    new: inputs to most methods can be paths OR images

--- opencv_utils.py
+++ opencv_utils.py
@@ -9,22 +9,52 @@ import sys
 
 BLEND_ALPHA = 0.5
 
+def get_image(path_or_image):
+    """ Returns an OpenCV image, whether a path or an image is provided.
+        Ensures that most methods can be used by passing the image path OR the image itself.
+        Parameters:
+        -----------
+        path_or_image: string or openCV image object
+            path or OpenCV image
+        returns:
+            loaded OpenCV image
+            (path_or_image if it already is an OpenCV image, 
+             a newly loaded one otherwise)
+
+    """
+    if type(path_or_image) is np.ndarray:
+        # openCV images are numpy arrays (TODO: more thorough check)
+        return path_or_image
+    else:
+        # path must have been provided (TODO: error handling)
+        return cv2.imread(path_or_image)
+        
 
 def concatenate_images(img1, img2):
     """ Concatanates two images horizontally
+        Parameters
+        ----------
+        img1: path or image
+        img2: path or image
     """
+    img1 = get_image(img1)
+    img2 = get_image(img2)
     # ensure that dimensions match, by converting all imgs to RGBA
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2BGRA)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2BGRA)
     return np.concatenate((img1, img2), axis=1)
 
 
-def make_transparent(annot_img):
+def get_transparent_img(img):
     """ Makes (black) annot bg transparent.
+        Parameters
+        ----------
+        img: image or path
     """
-    tmp = cv2.cvtColor(annot_img, cv2.COLOR_BGR2GRAY)
+    img = get_image(img)
+    tmp = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
-    b, g, r = cv2.split(annot_img)
+    b, g, r = cv2.split(img)
     rgba = [b, g, r, alpha]
     dst = cv2.merge(rgba, 4)
     return dst
@@ -34,13 +64,14 @@ def show_image(image, title, pos=None):
     """ Shows image with option to set position and enabling ESCAPE to quit.
         Parameters:
         -----------
-        image: object
+        image: object or path
             OpenCv iamge
         title: string
             window title
         pos: dict of integers
             {"x": x_pos, "y": y_pos}
     """
+    image = get_image(image)
     cv2.imshow(title, image)
     if pos is not None:
         cv2.moveWindow(title, pos["x"], pos["y"])
@@ -51,7 +82,12 @@ def show_image(image, title, pos=None):
 
 def add_text_to_image(img, txt, x_pos=10, y_offset=10):
     """ Adds text to an opencv image.
+        Parameters
+        ----------
+        img: path or opencv image
+        txt: string
     """
+    img = get_image(img)
     height, width, channels = img.shape
     font = cv2.FONT_HERSHEY_SIMPLEX
     bottomLeftCornerOfText = (x_pos, height - y_offset)
@@ -65,25 +101,27 @@ def add_text_to_image(img, txt, x_pos=10, y_offset=10):
                 fontScale,
                 fontColor,
                 lineType)
+    return img
 
 
 # maybe better: https://stackoverflow.com/questions/51365126/combine-2-images-with-mask
-def overlay_image(img_bg_path, img_overlay_path):
+def overlay_image(img_bg, img_overlay):
     """ Overlays an image over another.
 
         Parameters:
         ----------
 
         img_bg: string
-            path to background image
+            background image (path or image itself)
         img_overlay: string
-            path to overlay image. All transparent areas must be black (0, 0, 0).
+            overlay image (path or image itself)
+            All transparent areas must be black (0, 0, 0)
     """
-    bg_img = cv2.imread(img_bg_path)
-    img_overlay = cv2.imread(img_overlay_path)
+    bg_img =  get_image(img_bg)
+    img_overlay = get_image(img_overlay)
 
     bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGR2BGRA)
-    img_overlay_rgba = make_transparent(img_overlay)
+    img_overlay_rgba = get_transparent_img(img_overlay)
     # blend_images
     beta = (1.0 - BLEND_ALPHA)
     result = cv2.addWeighted(bg_img, BLEND_ALPHA, img_overlay_rgba, beta, 0.0)
