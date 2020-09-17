7e38d5359bda4f1a764aef0d4e9cc8baf9344b6d
unknown <amplejoe@gmail.com>
2020-09-17 23:57:51 +0200

    new function: clear dir contents without removing the dir itself

--- utils.py
+++ utils.py
@@ -267,6 +267,23 @@ def remove_dir(path):
         shutil.rmtree(path, ignore_errors=True)  # ignore errors on windows
 
 
+def clear_directory(path):
+    """Clears content of a directory without deleting the directory itself.
+
+    Args:
+        path ([type]): [description]
+    """    
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
 def get_directories(directory):
     retList = []
     for root, dirs, files in os.walk(directory):
