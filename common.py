74c6f3b27e01c8e380db4a1ad0e4bd9895d17bf3
unknown <amplejoe@gmail.com>
2020-11-03 22:23:53 +0100

    refactorings

--- common.py
+++ common.py
@@ -5,24 +5,31 @@ DEFAULT_CFG_FILE = "config.json"
 DEFAULT_IMG_DIRS = ['img', 'imag', 'frame', 'pic', 'phot']
 DEFAULT_ANNOT_DIRS = ['annot', 'sketch', 'mask']
 
+ANNOTATION_KEY = "annotations"
+IMAGE_KEY = "images"
+
 def get_ds_config(ds_root, *, has_annots=False):
     cfg_location = utils.join_paths_str(ds_root, DEFAULT_CFG_FILE)
     cfg = utils.read_json(cfg_location)
     cfg_update = {}
 
-    if ("images" not in cfg):
-        cfg_update["images"] = utils.prompt_folder_confirm(ds_root, DEFAULT_IMG_DIRS, "images")
+    if (IMAGE_KEY not in cfg):
+        cfg_update[IMAGE_KEY] = utils.prompt_folder_confirm(ds_root, DEFAULT_IMG_DIRS, IMAGE_KEY)
 
-    if has_annots:
-        if ("images" not in cfg):
-            ret["annotations"] = utils.join_paths(ds_root, cfg["annotations"])
-            cfg_update["annotations"] = utils.prompt_folder_confirm(ds_root, DEFAULT_ANNOT_DIRS, "annotations")
+    
+        if (ANNOTATION_KEY not in cfg):
+            ret[ANNOTATION_KEY] = utils.join_paths(ds_root, cfg[ANNOTATION_KEY])
+            cfg_update[ANNOTATION_KEY] = utils.prompt_folder_confirm(ds_root, DEFAULT_ANNOT_DIRS, ANNOTATION_KEY)
 
     utils.update_config_file(cfg_location, cfg_update)
            
     # re-read pot. changed config
     cfg = utils.read_json(cfg_location)
-    # runtime parameter (config's path)
+
+    # runtime parameters (never saved to file!!)
     cfg['config_path'] = cfg_location
+    cfg[f'{IMAGE_KEY}_full'] = utils.join_paths(ds_root, cfg[IMAGE_KEY])
+    if has_annots:
+        cfg[f'{ANNOTATION_KEY}_full'] = utils.join_paths(ds_root, cfg[ANNOTATION_KEY])
  
     return cfg
