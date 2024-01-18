00e9068245c80214342260997b658dac1453b79d
amplejoe <git.turmoil215@simplelogin.fr>
2024-01-18 17:53:07 +0100

    update info

--- DotDict.py
+++ DotDict.py
@@ -2,7 +2,7 @@ class DotDict(dict):
     """dot.notation access to dictionary attributes
     src: https://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary
     usage:
-        my_dict = dotdict({"val1": 1, "val2": 2})
+        my_dict = DotDict({"val1": 1, "val2": 2})
         print(my_dict.val1)
     """
 
